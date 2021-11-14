49d755552ed3544e6306bb8ed07a672537d89d5e
Venkatesh Macha <mvenkatesh431@gmail.com>
2021-11-14 15:53:26 +0530

    Refactored the code - Moved code to functions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -15,21 +16,42 @@ func main() {
 	urlVar := flag.String("url", "http://www.pybuzz.com/", "Website URL to create the Sitemap")
 	flag.Parse()
 
-	fmt.Printf("Site URL : %v\n", *urlVar)
-
-	resp, err := http.Get(*urlVar)
-	if err != nil {
-		log.Fatalf("Failed to retrive the webpage %v\n", err)
-	}
-	defer resp.Body.Close()
-
-	links, err := link.GetLinks(resp.Body)
+	siteLinks, err := get(*urlVar)
 	if err != nil {
 		log.Fatalf("Failed to get the links %v\n", err)
 	}
 
+	fmt.Println(siteLinks)
+}
+
+/*
+	The 'get' function will get the 'urlVar' webpage using the 'http.get'
+	Then we will parse the all the links using the 'LinkParser'
+*/
+func get(urlVar string) ([]string, error) {
+	resp, err := http.Get(urlVar)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
 	// This will help us to get the base url, Even though user provides the some page link(example.com/about-us)
 	baseUrl := resp.Request.URL.Scheme + "://" + resp.Request.URL.Host
+
+	return (getHrefs(resp.Body, baseUrl))
+}
+
+/*
+	The 'getHrefs' will take the 'io.Reader'(resp.Body) and baseUrl
+	Parses the links and returns the slice of strings(links)
+*/
+func getHrefs(reader io.Reader, baseUrl string) ([]string, error) {
+
+	// Parse the links from the webpage using 'https://github.com/mvenkatesh431/LinkParser'
+	links, err := link.GetLinks(reader)
+	if err != nil {
+		return nil, err
+	}
 
 	var siteLinks []string
 
@@ -43,12 +65,12 @@ func main() {
 			siteLinks = append(siteLinks, baseUrl+link.Href)
 
 		case strings.HasPrefix(link.Href, "http"):
-			// This will filter all valid links.
+			// This will filter all valid links(http and https)
 			if strings.HasPrefix(link.Href, baseUrl) {
+				// SiteMap contains baseUrl links only, So add only 'baseUrl' links to sitemap
 				siteLinks = append(siteLinks, link.Href)
 			}
 		}
 	}
-
-	fmt.Println(siteLinks)
+	return siteLinks, nil
 }
